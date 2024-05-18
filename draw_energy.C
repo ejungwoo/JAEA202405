cb878a978c2f748464e409e5dd5db13f5950458c
DAQ User <daquser@localhost.localdomain>
2024-05-18 16:06:24 +0900

    remove draw

--- draw_energy.C
+++
@@ -1,8 +0,0 @@
-void draw_energy(int runNumber=3)
-{
-  auto ana = new Analysis();
-
-  //ana -> InitializeDrawing();
-  ana -> ReadSummaryFile(Form("out/RUN%03d.summary.root",runNumber));
-  ana -> AnalyzeChannelEnergy(1,true);
-}
